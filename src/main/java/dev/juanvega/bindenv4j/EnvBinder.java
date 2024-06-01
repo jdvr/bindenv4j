4dde4623424cac89a0e098a5cf5dd349d85c7d06
jvegarodriguez <jvegarodriguez@newrelic.com>
2024-06-01 14:50:39 +0200

    refactor(EnvBinder): Change support for records

--- src/main/java/dev/juanvega/bindenv4j/EnvBinder.java
+++ src/main/java/dev/juanvega/bindenv4j/EnvBinder.java
@@ -1,8 +1,5 @@
 package dev.juanvega.bindenv4j;
 
-import com.sun.jdi.Value;
-
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
@@ -25,55 +22,42 @@ public class EnvBinder {
      * @param classType The class type to be inspected for fields.
      */
     public <T> T bind(Class<T> classType) {
-        T instance = null;
+
         try {
-            var constructors = classType.getDeclaredConstructors();
-            var targetConstructor = constructors[0];
-            for (Constructor<?> constructor : constructors) {
-                if (constructor.equals(targetConstructor)) {
-                    continue;
-                }
-                if (targetConstructor.getParameterCount() > constructor.getParameterCount()) {
-                    targetConstructor = constructor;
-                }
-            }
-            targetConstructor.setAccessible(true);
-            if (targetConstructor.getParameterCount() > 0) {
-                var valuesByField = fieldValues(classType).entrySet();
+            if (classType.isRecord()) {
+                var constructors = classType.getDeclaredConstructors();
+                var targetConstructor = constructors[0];
+                var valuesByField = fieldValuesByName(classType);
+                targetConstructor.setAccessible(true);
                 Parameter[] parameters = targetConstructor.getParameters();
                 Object[] parameterValues = new Object[parameters.length];
                 for (var i = 0; i < parameters.length; i++) {
                     var parameter = parameters[i];
                     var targetParameterName = parameter.getName();
-                    var field = valuesByField.stream().filter(
-                            entry -> entry.getKey().getName().equals(targetParameterName)
-                    ).findFirst().orElseThrow(
-                            () -> new IllegalStateException(
-                            "Unable to find field for constructor parameter %s".formatted(targetParameterName))
-                    );
-                    parameterValues[i] = field.getValue();
+                    if (!valuesByField.containsKey(targetParameterName)) {
+                        throw new IllegalStateException(
+                                "Unable to find field for constructor parameter %s".formatted(targetParameterName)
+                        );
+                    }
+                    parameterValues[i] = valuesByField.get(targetParameterName);
                 }
                 return (T) targetConstructor.newInstance(parameterValues);
+            } else {
+                T instance = classType.newInstance();
+                var valuesByField = fieldValues(classType);
+                for (Map.Entry<Field, Object> valueByField : valuesByField.entrySet()) {
+                    valueByField.getKey().setAccessible(true);
+                    valueByField.getKey().set(instance, valueByField.getValue());
+                }
+                return instance;
             }
-            instance = (T) targetConstructor.newInstance();
-        } catch (InstantiationException e) {
+
+
+        } catch (InstantiationException | InvocationTargetException |
+                 IllegalAccessException e) {
             throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-        var valuesByField = fieldValues(classType);
-        for (Map.Entry<Field, Object> valueByField : valuesByField.entrySet()) {
-            valueByField.getKey().setAccessible(true);
-            try {
-                valueByField.getKey().set(instance, valueByField.getValue());
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
         }
 
-        return instance;
     }
 
     private <T> Map<Field, Object> fieldValues(Class<T> classType) {
@@ -93,10 +77,19 @@ public class EnvBinder {
                     throw new IllegalStateException("No value for " + field.getName());
                 }
 
-                return  null;
+                return null;
             });
             result.put(field, value);
         }
         return result;
+    }
+
+    private <T> Map<String, Object> fieldValuesByName(Class<T> classType) {
+        var entries = fieldValues(classType).entrySet()
+                .stream()
+                .map(entry -> Map.entry(entry.getKey().getName(), entry.getValue()))
+                .toArray(Map.Entry[]::new);
+        return Map.ofEntries(entries);
+
     }
 }
